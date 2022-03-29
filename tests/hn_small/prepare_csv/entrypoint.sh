251bad40fec6afa8ea0d2eab9aa4154ae95edddb
Sergey Nikolaev <prostuda@academ.org>
2022-03-29 15:22:22 +0200

    fixed hn* tests

--- tests/hn_small/prepare_csv/entrypoint.sh
+++ tests/hn_small/prepare_csv/entrypoint.sh
@@ -6,7 +6,6 @@
 echo "Cleaning";
 cat /data/downloaded.csv | tr -cd '\11\12\15\40-\176' > /data/cleaned.csv
 echo "Preparing"
-rm /data/data.csv 2>/dev/null
-csvcut -e utf-8 -l -c 0,3,4,5,6,7,8,9,10 -z 1073741824 /data/cleaned.csv|grep -v author_comment_count|csvformat -U1 -z 1073741824 > /data/data.csv
+csvcut -e utf-8 -l -c 1,4,5,6,7,8,9,10,11 -z 1073741824 /data/cleaned.csv|grep -v author_comment_count|csvformat -U1 -z 1073741824 > /data/data.csv
 rm /data/cleaned.csv
 
